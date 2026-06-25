d3561d7d1dc8674a1ba3e51f79268710f3decf64
dri-y <adriandiluka@gmail.com>
2026-06-25 00:07:03 -0300

    Refactor Cofre class and add user sorting
    Refactor Cofre class to use a fixed-size array for users and add user sorting functionality.

--- cofre/Cofre.h
+++ cofre/Cofre.h
@@ -1,27 +1,39 @@
 #ifndef COFRE_H
 #define COFRE_H
-#include <vector>
+
+#include <iostream>
 #include "Usuario.h"
 using namespace std;
+
 class Cofre {
 private:
-    vector<Usuario> usuarios;
+    static int total_usuarios = 100;
+    Usuario usuarios[total_usuarios];
+    int qtdUsuarios;
     bool aberto;
 public:
     Cofre() {
         aberto = false;
+        qtdUsuarios = 0;
     }
     
-    void adicionarUsuario(Usuario usuario) {
-        usuarios.push_back(usuario);
+    void adicionarUsuario(Usuario user) {
+        if(qtdUsuarios < total_usuarios){
+        	usuarios[qtdUsuarios] = user;
+        	qtdUsuarios++;
+        	return true;
+		}
+		return false;
     }
     
-    vector<Usuario> getUsuarios() {
+    Usuario* getUsuarios() {
         return usuarios;
     }
-    
+    int getqtdUsuarios(){
+    	return qtdUsuarios;
+	}
     bool autenticar(int id, string senha) {
-        for(int i = 0; i < usuarios.size(); i++) {
+        for(int i = 0; i < qtdUsuarios; i++) {
             if(usuarios[i].getId() == id &&
                usuarios[i].verificarSenha(senha)) {
                 return true;
@@ -30,7 +42,7 @@ public:
         return false;
     }
     bool validarAdmin(int id, string senha) {
-       for (int i = 0; i < usuarios.size(); i++) {
+       for (int i = 0; i < qtdUsuarios; i++) {
            if (usuarios[i].getId() == id && usuarios[i].getSenha() == senha) {
             return usuarios[i].isAdmin();
            }
@@ -49,5 +61,16 @@ public:
     bool estaAberto() {
         return aberto;
     }
+    void ordenarUsuariosPorId(){
+    	for(int i=0;i< qtdUsuarios-1; i++){
+    		for(int j=0; j < qtdUsuarios-i-1; j++){
+    			if(usuarios[j]).getId()>usuarios[j+1].getId()){
+    				Usuario temp = usuarios[j];
+    				usuarios[j] = usuarios[j+1];
+    				usuarios[j+1] = temp;
+				}
+			}
+		}
+	}
 };
 #endif
