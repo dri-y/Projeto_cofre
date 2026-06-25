d2c058efa69eb7dd0192c1b69478bb9942d006d8
dri-y <adriandiluka@gmail.com>
2026-06-25 10:46:52 -0300

    Create Display.h for LCD display management
    Added Display class for managing LCD display interactions.

---
+++ Wowki/Display.h
@@ -0,0 +1,43 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include "Usuario.h"
+
+class Display {
+private:
+    LiquidCrystal_I2C lcd;
+
+public:
+    Display() : lcd(0x27, 20, 4) {}
+
+    void iniciar() {
+        lcd.init();
+        lcd.backlight();
+    }
+
+    void mostrar(String texto) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print(texto);
+    }
+
+    void listaUsuarios(Usuario* usuarios, int qtdUsuarios) {
+        lcd.clear();
+
+        for (int i = 0; i < qtdUsuarios; i++) {
+            lcd.clear();
+            lcd.setCursor(0, 0);
+            lcd.print("ID:");
+            lcd.print(usuarios[i].getId());
+
+            lcd.setCursor(0, 1);
+            lcd.print(usuarios[i].getNome());
+
+            delay(2000); // Exibe cada usuário por 2 segundos
+        }
+    }
+};
+
+#endif
