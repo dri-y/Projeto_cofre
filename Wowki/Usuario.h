5bfb3025d87cf10a7f9c800d710081d0422065e0
dri-y <adriandiluka@gmail.com>
2026-06-25 10:47:54 -0300

    Add Usuario class with user management features

---
+++ Wowki/Usuario.h
@@ -0,0 +1,46 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+#include <string>
+using namespace std;
+class Usuario {
+private:
+    int id;
+    string nome;
+    string senha;
+    bool admin;
+
+public:
+	Usuario() {
+    id = 0;
+    nome = "";
+    senha = "";
+    admin = false;
+}
+    Usuario(int id, string nome, string senha, bool admin = false) {
+        this->id = id;
+        this->nome = nome;
+        this->senha = senha;
+        this->admin = admin;
+    }
+    
+    int getId() {
+        return id;
+    }
+
+    string getNome() {
+        return nome;
+    }
+
+    string getSenha() {
+        return senha;
+    }
+
+    bool isAdmin() {
+        return admin;
+    }
+    
+    bool verificarSenha(string tentativa) {
+        return senha == tentativa;
+    }
+};
+#endif
