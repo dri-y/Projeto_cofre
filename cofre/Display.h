6d84d6e70c471b18929b927b74bb6a22c7bbe28e
dri-y <adriandiluka@gmail.com>
2026-06-25 10:33:41 -0300

    Change ListaUsuarios to accept pointer and count

--- cofre/Display.h
+++ cofre/Display.h
@@ -1,7 +1,6 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 #include <iostream>
-#include <vector>
 #include "Usuario.h"
 #include "Cofre.h"
 using namespace std;
@@ -10,10 +9,10 @@ public:
     void mostrar(string texto) {
         cout << texto << endl;
     }
-    void ListaUsuarios(vector<Usuario> usuarios) {
-    cout << "\n=== USUÁRIOS CADASTRADOS ===\n";
+    void ListaUsuarios(Usuario* usuarios, int qtdUsuarios) {
+    cout << "\n=== USUARIOS CADASTRADOS ===\n";
 
-    for (int i = 0; i < usuarios.size(); i++) {
+    for (int i = 0; i < qtdUsuarios; i++) {
     cout
         << "ID: " << usuarios[i].getId()
         << " | Nome: " << usuarios[i].getNome()
